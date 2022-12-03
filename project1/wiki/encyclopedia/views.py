89a3f700d9294a474885ed1ae67f63d4df5d09bb
moshe hadad <moshe4631h@gmail.com>
2022-12-04 01:33:44 +0200

    add search bar

--- project1/wiki/encyclopedia/views.py
+++ project1/wiki/encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse 
 from . import util
 import markdown2
 
@@ -10,6 +10,9 @@ def index(request):
     })
 
 def entry(request, name):
+    '''
+    find entry and render web page
+    '''
     entry = util.get_entry(name)
     if entry == None:
         return HttpResponse("entry not found")
@@ -19,3 +22,34 @@ def entry(request, name):
         return render(request, "encyclopedia/entry.html", {
           'entry' : html_entry , 'title' : name
         })
+
+def search(request):
+    '''
+    search bar    
+    '''
+    if request.method == "POST":
+        q = request.POST["q"]
+        entry = util.get_entry(q)
+        # if search dont found
+        if entry == None:
+            L = []
+            list_name = util.list_entries()
+            for i in list_name:
+                if q.lower() in i.lower():
+                    print(type(q))
+                    print(type(i))
+                    L.append(i)
+            return render(request, "encyclopedia/search.html", {
+                "entries": L
+            })
+        # if search found
+        else:
+            html_entry = markdown2.markdown(entry)
+            
+            return render(request, "encyclopedia/entry.html", {
+            'entry' : html_entry , 'title' : q
+            })
+    else:
+        return render(request, "encyclopedia/search.html", {
+        })
+
