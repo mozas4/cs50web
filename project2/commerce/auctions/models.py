0cad9141df1e44470395beb6e099e510ed934c68
moshe hadad <moshe4631h@gmail.com>
2022-12-14 16:03:30 +0200

    creating new branch

--- project2/commerce/auctions/models.py
+++ project2/commerce/auctions/models.py
@@ -4,3 +4,14 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing():
+    owner = models.ForeignKey(User, on_delete=models.CASCADE related_name="owner")
+    title = models.CharField(max_length=64)
+    des = models.CharField()
+    bid = models.IntegerField()
+    url = models.CharField()
+    category = models.CharField() 
+
+    def __str__(self):
+        return f"listing of {self.title} belongs to {self.owner}"
